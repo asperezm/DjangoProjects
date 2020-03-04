1b2733acd2a62d733612d67925066c8aa107afc4
asperezm <47333056+asperezm@users.noreply.github.com>
2020-03-03 17:32:30 -0800

    Taller 1

--- measure/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-from rest_framework import viewsets
-from .models import Measure
-from .serializers import MeasureSerializer
-
-class MeasureViewSet(viewsets.ModelViewSet):
-    queryset = Measure.objects.all().order_by('-created')
-    serializer_class = MeasureSerializer
