71cad68c8d6a441c3e29053f4f4a82e60466233a
asperezm <47333056+asperezm@users.noreply.github.com>
2020-05-12 21:06:13 -0500

    "cambios necesarios"

--- DjangoProjects/settings.py
+++ DjangoProjects/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'DjangoProjects.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django',
+        'USER': 'root',
+        'PASSWORD': '1626',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
